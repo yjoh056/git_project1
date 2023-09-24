ca237848f99e75e1142ee3b94b25aa0f68528285
yjoh056 <yjoh056@gmail.com>
2023-09-24 11:40:21 +0900

    hero-banner

--- js/basic.js
+++ js/basic.js
@@ -11,12 +11,3 @@ skipNavi.addEventListener('blur', function() {
     this.style.transform = 'translateY(-200%)';
 });
 
-// 
-/* BX slider*/ 
-// 
-$('.slid_list').bxSlider({
-	mode: 'fade',
-	speed: 500,
-	auto: true,
-	autoHover: true,
-});
