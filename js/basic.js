ba744125bd4d467fac60d21558afb6412e52b0f3
yjoh056 <yjoh056@gmail.com>
2023-10-06 10:02:48 +0900

    업데이트1006

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,5 @@
-//
+$(() => {
+    //
 /* skip navi */ 
 // 
 const skipNavi = document.querySelector('#skip_navi a');
@@ -11,3 +12,14 @@ skipNavi.addEventListener('blur', function() {
     this.style.transform = 'translateY(-200%)';
 });
 
+//
+/*  */ 
+// 
+const starBtnL = document.getElementsByClassName('.star-left');
+window.addEventListener('wheel', handleMouseWheel);
+function handleMouseWheel(event) {
+    const delta = event.deltaY;
+    const rotationAngle = delta * 0.1;
+    starBtnL.style.transform = `rotateZ(${rotationAngle}deg)`;
+}
+});
