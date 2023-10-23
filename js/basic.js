cb050512f2a1fdd95c076e6ae42c634a6a5ce2dd
yjoh056 <yjoh056@gmail.com>
2023-10-23 21:56:10 +0900

    슬라이드추가

--- js/basic.js
+++ js/basic.js
@@ -1,25 +1,44 @@
 $(() => {
     //
-/* skip navi */ 
-// 
-const skipNavi = document.querySelector('#skip_navi a');
+    /* skip navi */
+    // 
+    const skipNavi = document.querySelector('#skip_navi a');
 
-skipNavi.addEventListener('focus', function() {
-    this.style.transform = 'translateY(100%)';
-});
+    skipNavi.addEventListener('focus', function () {
+        this.style.transform = 'translateY(100%)';
+    });
 
-skipNavi.addEventListener('blur', function() {
-    this.style.transform = 'translateY(-200%)';
-});
+    skipNavi.addEventListener('blur', function () {
+        this.style.transform = 'translateY(-200%)';
+    });
 
-//
-/*  */ 
-// 
-const starBtnL = document.getElementsByClassName('.star-left');
-window.addEventListener('wheel', handleMouseWheel);
-function handleMouseWheel(event) {
-    const delta = event.deltaY;
-    const rotationAngle = delta * 0.1;
-    starBtnL.style.transform = `rotateZ(${rotationAngle}deg)`;
-}
+    /* slide */
+    
+    $('.slid_list').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnDotsHover: true,
+        speed: 2000,
+        prevArrow: $('.hero-control .pre'),
+        nextArrow: $('.hero-control .next'),
+        pauseOnHover:$('.hero-control .purse'),
+    });
+
+    $('.card-slot').slick({
+        infinite: true,
+        slidesToShow: 5,
+        pauseOnDotsHover: true,
+        prevArrow: $('.slot-box .pre'),
+        nextArrow: $('.slot-box .next'),
+    });
+    $('.photo_lists').slick({
+        infinite: true,
+        autoplay: false,
+        slidesToShow: 1,
+        pauseOnDotsHover: true,
+        prevArrow: $('.slide_wrap .pre'),
+        nextArrow: $('.slide_wrap .next'),
+    });
+
 });
